2d0aab937335f66c19e17a5e6d91468ba02f3810
Blito <pabloarubi@gmail.com>
2015-06-24 12:28:11 -0300

    SFML and VTK working together <3

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@
 // SFML INCLUDES
 #include <SFML/Graphics.hpp>
 
+#include <thread>
+
 // VTK variables
 constexpr float planeWidth = 200.0f;
 constexpr float planeHeight = 100.0f;
@@ -30,18 +32,17 @@ constexpr unsigned int textureWidth = 50;
 constexpr unsigned int textureHeight = 100;
 constexpr char * fontPath = "arial.ttf";
 
-int main()
+void sfml()
 {
-	// ########### SETUP SFML ########### 
+    // Setup
     sf::RenderWindow window;
     window.create(sf::VideoMode(sfmlWidth, sfmlHeight), "SFML Window");
 
     // Create texture buffer
     texture_raw = new unsigned char[textureWidth * textureHeight * 4]; // RGBA
-    for (unsigned int i = 0; i < textureWidth * textureHeight * 4; i+=4)
+    for (unsigned int i = 0; i < textureWidth * textureHeight * 4; i++)
     {
-    	texture_raw[i] = 255; // red square
-    	texture_raw[i+3] = 255;
+        texture_raw[i] = 255; // red square
     }
 
     // Create texture
@@ -52,26 +53,56 @@ int main()
     // Create sprite
     sf::Sprite sprite;
     sprite.setTexture(texture);
-    unsigned int x = 50;
-    unsigned int y = 100;
+    unsigned int x = 150;
+    unsigned int y = 30;
     sprite.setPosition(x, y);
 
     // Create FPS output
     sf::Text fpsOutput;
     sf::Font font;
     if (!font.loadFromFile(fontPath))
-        return -1;
+        return;
     fpsOutput.setFont(font);
     fpsOutput.setCharacterSize(24);
     fpsOutput.setColor(sf::Color::Red);
     fpsOutput.setPosition(0, sfmlHeight-30);
-	// ########### END SETUP SFML ###########
 
-	// ########### SETUP VTK ###########
-	auto renderer = vtkRenderer::New();
-    renderer->SetBackground(.05,.1,.15);
+    // Render
+    auto startTime = std::chrono::high_resolution_clock::now();
+    std::chrono::time_point<std::chrono::high_resolution_clock> endTime;
+    while (true)
+    {
+        // Event loop
+        sf::Event event;
+        while (window.pollEvent(event)) {}
 
-	// Create render window
+        // Update FPS
+        endTime = std::chrono::high_resolution_clock::now();
+        double fps = (double)1000.0/std::chrono::duration_cast<std::chrono::milliseconds>(endTime-startTime).count();
+        std::ostringstream strs;
+        strs << "FPS: " << fps;
+        std::string str = strs.str();
+        fpsOutput.setString(str);
+        startTime = std::chrono::high_resolution_clock::now();
+
+        // Update square
+        sprite.setPosition(x, y);
+        x < sfmlWidth - textureWidth ? x++ : x = 0;
+        y < sfmlHeight - textureHeight ? y++ : y = 0;
+
+        window.clear();
+        window.draw(fpsOutput);
+        window.draw(sprite);
+        window.display();
+    }
+}
+
+void vtk()
+{
+    // Setup
+    auto renderer = vtkRenderer::New();
+
+    // Create render window
     auto renWin = vtkRenderWindow::New();
     renWin->AddRenderer(renderer);
     renWin->SetSize(600,600);
@@ -81,9 +112,10 @@ int main()
     camera->SetPosition(50, 50, 100);
     camera->SetFocalPoint(0, 0, 0);
     renderer->SetActiveCamera(camera);
+    renderer->SetBackground(.05,.1,.15);
 
-	// Create a plane
-	auto texturedPlane = vtkActor::New();
+    // Create a plane
+    auto texturedPlane = vtkActor::New();
     auto plane = vtkSmartPointer<vtkPlaneSource>::New();
 
     plane->SetOrigin(0, planeHeight, 0);
@@ -98,46 +130,26 @@ int main()
     texturedPlane->SetOrigin(planeWidth / 2,
                              planeHeight, 0);
 
-	renderer->AddActor(texturedPlane);
+    renderer->AddActor(texturedPlane);
     renderer->ResetCamera();
-    // ########### END SETUP VTK ###########
 
-    auto startTime = std::chrono::high_resolution_clock::now();
-    std::chrono::time_point<std::chrono::high_resolution_clock> endTime;
-    while (true)
+    // Render
+    while(true)
     {
-	    // ########### UPDATE VTK ###########
-		texturedPlane->SetOrientation(0,0,0);
-	    texturedPlane->RotateY(rot_x++);
-	    texturedPlane->RotateX(rot_y--);
-	    texturedPlane->RotateZ(rot_z++);
+        texturedPlane->SetOrientation(0,0,0);
+        texturedPlane->RotateY(rot_x++);
+        texturedPlane->RotateX(rot_y--);
+        texturedPlane->RotateZ(rot_z++);
+            
+        renWin->Render();
+    }
+}
 
-	    renWin->Render();
-	    // ########### END UPDATE VTK ###########
+int main()
+{
+    std::thread sfml_thread(sfml);
+    std::thread vtk_thread(vtk);
 
-	    // ########### UPDATE SFML ###########
-	    // Event loop
-		sf::Event event;
-        while (window.pollEvent(event)) {}
-
-	    // Update FPS
-	    endTime = std::chrono::high_resolution_clock::now();
-	    double fps = (double)1000.0/std::chrono::duration_cast<std::chrono::milliseconds>(endTime-startTime).count();
-	    std::ostringstream strs;
-	    strs << "FPS: " << fps;
-	    std::string str = strs.str();
-	    fpsOutput.setString(str);
-	    startTime = std::chrono::high_resolution_clock::now();
-
-	    // Update square
-	    sprite.setPosition(x, y);
-	    x < sfmlWidth - textureWidth ? x++ : x = 0;
-	    y < sfmlHeight - textureHeight ? y++ : y = 0;
-
-	    window.clear();
-	    window.draw(fpsOutput);
-    	window.draw(sprite);
-    	window.display();
-		// ########### END UPDATE SFML ###########
-	}
+    sfml_thread.join();
+    vtk_thread.join();
 }
