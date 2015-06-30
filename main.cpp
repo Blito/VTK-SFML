0d5199e4feb4ba4dcc88039b7bd68784bec900a4
Blito <pabloarubi@gmail.com>
2015-06-30 14:10:25 -0300

    Fixed! Thanks to asdfasdf@StackOverflow

--- main.cpp
+++ main.cpp
@@ -2,28 +2,52 @@
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkActor.h>
-#include <vtkCamera.h>
-#include <vtkSmartPointer.h>
 #include <vtkPlaneSource.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkCommand.h>
 
 // VTK variables
 constexpr float planeWidth = 200.0f;
 constexpr float planeHeight = 100.0f;
+vtkActor * texturedPlane;
+vtkRenderWindowInteractor * renderWindowInteractor;
+vtkRenderWindow * renWin;
+float rot = 0.0f;
+
+class RotateCommand : public vtkCommand
+{
+public:
+    vtkTypeMacro(RotateCommand, vtkCommand);
+
+    static RotateCommand * New()
+    {
+        return new RotateCommand;
+    }
+
+    void Execute(vtkObject * vtkNotUsed(caller),
+                 unsigned long vtkNotUsed(eventId), 
+                 void * vtkNotUsed(callData))
+    {
+        texturedPlane->SetOrientation(0,0,0);
+        texturedPlane->RotateZ(rot++);
+            
+        renWin->Render();
+    }
+};
 
 int main()
 {
     auto renderer = vtkRenderer::New();
 
     // Create render window
-    auto renWin = vtkRenderWindow::New();
+    renWin = vtkRenderWindow::New();
     renWin->AddRenderer(renderer);
     renWin->SetSize(600,600);
 
     // Create a plane
-    auto texturedPlane = vtkActor::New();
+    texturedPlane = vtkActor::New();
     auto plane = vtkPlaneSource::New();
     plane->SetOrigin(0, planeHeight, 0);
     plane->SetPoint1(planeWidth, planeHeight, 0);
@@ -35,20 +59,16 @@ int main()
     texturedPlane->SetOrigin(planeWidth / 2, planeHeight, 0);
 
     renderer->AddActor(texturedPlane);
+
     renderer->ResetCamera();
 
     // Create a RenderWindowInteractor
-    auto renderWindowInteractor = vtkRenderWindowInteractor::New();
+    renderWindowInteractor = vtkRenderWindowInteractor::New();
     renderWindowInteractor->SetRenderWindow(renWin);
-    renderWindowInteractor->Start();
+    renderWindowInteractor->Initialize();
+    renderWindowInteractor->CreateRepeatingTimer(1);
+    RotateCommand * rotateCallback =  RotateCommand::New();
+    renderWindowInteractor->AddObserver(vtkCommand::TimerEvent, rotateCallback );
 
-    // Render
-    float rot = 0.0f;
-    while(true)
-    {
-        texturedPlane->SetOrientation(0,0,0);
-        texturedPlane->RotateZ(rot++);
-            
-        renWin->Render();
-    }
+    renderWindowInteractor->Start();
 }
